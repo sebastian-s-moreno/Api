aeae81287a30a3b795cdc5ba7ac823b17354a98d
skjalgw <skjalgwinnerdal@gmail.com>
2022-01-13 09:58:15 +0100

    Misc

--- src/Forte.Weather.Api/Controllers/WeatherController.cs
+++ src/Forte.Weather.Api/Controllers/WeatherController.cs
@@ -1,5 +1,7 @@
 using Forte.Weather.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Net.Http.Headers;
 
 namespace Forte.Weather.Api.Controllers
 {
@@ -23,6 +25,41 @@ namespace Forte.Weather.Api.Controllers
             return Locations[index];
         }
 
+        [HttpGet("locations/recommended2")]
+        public Location? GetRecommended2(string preference)
+        {
+            Location location = null;
+            if (Locations.Count() > 0)
+            {
+                switch (preference)
+                {
+                    case "Swimming":
+                        location = Locations.OrderByDescending(x => x.Timeserie?.Data.Instant.Details.Air_temperature).First();
+                        break;
+                    case "Sailing":
+                        location = Locations.OrderByDescending(x => x.Timeserie?.Data.Instant.Details.Wind_speed).First();
+                        break;
+                    case "Skiing":
+                        location = Locations.OrderBy(x => x.Timeserie?.Data.Instant.Details.Air_temperature).First();
+                        break;
+                    case "Sightseeing":
+                        location = Locations.OrderBy(x => x.Timeserie?.Data.Instant.Details.Air_pressure_at_sea_level).First();
+                        break;
+                    case "Unspecified":
+                        var index = new Random().Next(Locations.Count);
+                        location = Locations[index];
+                        break;
+
+                }
+                return location;
+
+            }
+            else
+            {
+                return null; //Returner feilmelding og si at man må legge til locations
+            }
+        }
+
         [HttpGet("locations")]
         public IEnumerable<Location> Get()
         {
@@ -30,17 +67,72 @@ namespace Forte.Weather.Api.Controllers
         }
 
         [HttpPost("locations")]
-        public OkResult Post([FromBody] Location location)
+        public async Task<OkResult> Post([FromBody] Location location)
         {
+            location.ID = Locations.Count();
+            double latitude = double.Parse(location.Latitude, CultureInfo.InvariantCulture);
+            double longitude = double.Parse(location.Longitude, CultureInfo.InvariantCulture);
+            TimeSerie? ts = await GetDetails(latitude, longitude);
+            location.Timeserie = ts;
             Locations.Add(location);
             return Ok();
         }
 
+        [HttpGet("details")]
+        public async Task<TimeSerie> GetDetails(double lat, double lon)
+        {
+            var elements = $"lat={lat}&lon={lon}";
+            var url = "https://api.met.no/weatherapi/locationforecast/2.0/compact?" + elements;
+            YrApiResponse? response = null;
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                var productValue = new ProductInfoHeaderValue("ForteWeatherApp", "1.0");
+                request.Headers.UserAgent.Add(productValue);
+                var httpResponse = await client.SendAsync(request);
+                response = await httpResponse.Content.ReadFromJsonAsync<YrApiResponse>();
+            }
+            return response?.Properties?.Timeseries?.FirstOrDefault();
+        }
+
         public class Location
         {
-            public string Name { get; set; }
-            public string Latitude { get; set; }
-            public string Longitude { get; set; }
+            public int ID { get; set; }
+            public string Name { get; set; } = "";
+            public string Latitude { get; set; } = "";
+            public string Longitude { get; set; } = "";
+            public TimeSerie? Timeserie { get; set; }
+        }
+
+        public class YrApiResponse
+        {
+            public string? Type { get; set; }
+            public Property Properties { get; set; } = new();
+        }
+        public class Property
+        {
+            public List<TimeSerie> Timeseries { get; set; } = new();
+        }
+        public class TimeSerie
+        {
+            public Data Data { get; set; } = new();
+            public DateTimeOffset Time { get; set; }
+        }
+        public class Data
+        {
+            public Instant Instant { get; set; } = new();
+        }
+        public class Instant
+        {
+            public Details Details { get; set; } = new();
+        }
+        public class Details
+        {
+            public double Air_pressure_at_sea_level { get; set; }
+            public double Air_temperature { get; set; }
+            public double Relative_humidity { get; set; }
+            public double Wind_from_direction { get; set; }
+            public double Wind_speed { get; set; }
         }
     }
 }
